e8954bf092d2baef16908d469406e144ef2dc85c
Caio2a7 <caiodanielfonseca@gmail.com>
2026-08-16 21:43:25 -0300

    Initialize Spring-Boot

---
+++ src/main/java/io/github/caio2a7/distributed_payment_broker/DistributedPaymentBrokerApplication.java
@@ -0,0 +1,13 @@
+package io.github.caio2a7.distributed_payment_broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedPaymentBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedPaymentBrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/caio2a7/distributed_payment_broker/DistributedPaymentBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.caio2a7.distributed_payment_broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedPaymentBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
