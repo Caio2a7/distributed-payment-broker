eaf1cdffb04acf4b69a11420250c8b95d46df3e9
Caio2a7 <caiodanielfonseca@gmail.com>
2026-08-19 00:04:02 -0300

    feat(server): implementar PaymentService e servidor PaymentBroker

---
+++ dpb-server/src/main/java/br/ufrn/dpb/server/service/PaymentService.java
@@ -0,0 +1,36 @@
+package br.ufrn.dpb.server.service;
+
+import br.ufrn.dpb.server.model.*;
+import br.ufrn.dpb.server.repository.*;
+import java.math.BigDecimal;
+
+
+public class PaymentService {
+    private final AccountRepository accountRepository;
+
+    public PaymentService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Transaction ProcessTransaction(Long sourceAccountId, Long destAccountId, BigDecimal amount){
+        if(sourceAccountId.equals(destAccountId)){
+            throw new IllegalArgumentException("As chave de destino não pode ser a mesma da chave de origem");
+        }
+        
+
+        Account source = accountRepository.findById(sourceAccountId).orElseThrow(() -> new IllegalArgumentException("Melhorar erro"));
+        Account destination = accountRepository.findById(destAccountId).orElseThrow(() -> new IllegalArgumentException("Melhorar erro"));
+
+        Transaction transaction = new Transaction(id, sourceAccountId, destAccountId, amount, TransactionStatus.PENDING);
+    
+        source.sacar(amount);
+        destination.depositar(amount);
+        accountRepository.save(source);
+        accountRepository.save(destination);
+
+        Long id = accountRepository.nextId();
+        transaction.setStatus(TransactionStatus.COMPLETED);
+
+        return transaction;
+    }
+}
