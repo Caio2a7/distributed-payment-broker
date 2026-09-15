2161ed697bf6f8b19c7e4c249e1910c900092431
Caio2a7 <caiodanielfonseca@gmail.com>
2026-09-15 13:30:00 -0300

    feat(server): inicializar aplicacao integrando PaymentService ao Broker UDP na Main

--- dpb-server/src/main/java/br/ufrn/dpb/server/Main.java
+++ dpb-server/src/main/java/br/ufrn/dpb/server/Main.java
@@ -1,6 +1,5 @@
 package br.ufrn.dpb.server;
 
-import java.math.BigDecimal;
 import br.ufrn.dpb.server.controller.PaymentController;
 import br.ufrn.dpb.server.network.UDPServer;
 import br.ufrn.dpb.server.repository.AccountRepository;
@@ -10,13 +9,19 @@ import br.ufrn.dpb.server.service.PaymentService;
 public class Main {
     public static void main(String[] args) {
         int port = 8080;
+        String hmacSecret = "12345";
+
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+            "%1$tT.%1$tL [\u001B[32m%4$s\u001B[0m] %3$s - %5$s%6$s%n");
+
+        Logger.getLogger("").setLevel(Level.OFF);
 
         AccountRepository accountRepository = new InMemoryAccountRepository();
         PaymentService paymentService = new PaymentService(accountRepository);
         PaymentController paymentController = new PaymentController(paymentService);
         UDPServer udpServer = new UDPServer(port, paymentController);
-        accountRepository.create(BigDecimal.valueOf(10000));
-        accountRepository.create(BigDecimal.valueOf(5000));
+        accountRepository.create(10000L);
+        accountRepository.create(1000L);
 
         udpServer.start();
     }
